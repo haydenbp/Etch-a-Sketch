4bcc144cd3d66707a1a6a294ac6111a3bbdf6383
haydenbp <32523980+haydenbp@users.noreply.github.com>
2021-01-12 17:19:16 -0500

    added buttons and clear function

--- Etch.js
+++ Etch.js
@@ -1,54 +1,79 @@
-
-
 const container = document.querySelector('container');
-
 const cells = document.querySelectorAll('cell');
 const slider = document.querySelector('input');
+const buttons = document.querySelectorAll('.buttons')
+const clear = document.querySelector('.clearButton');
+let color = 'black';
+
+console.log(clear)
+
+buttons.forEach(item => item.addEventListener('click',
+
+()=>{
+    color = item.innerText ;
+    console.log(typeof(color));
+}))
+
+console.log(color)
+
+/*colorChange(){
+    switch (color) {
+        case value:
+            
+            break;
+
+        default: black
+            break;
+    }
 
 
+}
+*/
 
-createGrid = gridNumber =>{
+createGrid = gridNumber =>{ // creates a grid with Height and Width of slider value
   let cellLength = `${100/gridNumber}%`
 
   let gridArea = gridNumber**2
 
-    while(gridArea>0){
+    while(gridArea>0){  //creates a div with event listener "hover" and appends to container 
+
     const cell = document.createElement('cell');
+
     container.appendChild(cell);
+
     cell.style.flexBasis = cellLength;
+
     cell.style.height = cellLength;
+
     cell.addEventListener("mouseover", event => {
-        cell.className = "hover"
+          //  cell.className = "hover"
+          cell.style.backgroundColor = color;
+          colorChange();
     })
+
     gridArea--;
     }
     
 };
 
 
-createGrid(slider.value);
+createGrid(slider.value); //calls createGrid function using initial slider value
 
 
-
-console.log(cells)
-
-
-
-resetGrid = () =>{
+resetGrid = () =>{ //sets grid dimensions to slider value
 
     let gridPixels = container.querySelectorAll('cell');
     console.log(gridPixels)
-  
 
-gridPixels.forEach(item =>item.remove());
-createGrid(slider.value);
+    gridPixels.forEach(item =>item.remove());
+    
+    createGrid(slider.value);
 
 };
 
 
 
-slider.addEventListener('change', ()=>{
-
+slider.addEventListener('change', ()=>{ //calls resetGrid function when slider value is changed
     
     resetGrid();
 
@@ -56,5 +81,11 @@ slider.addEventListener('change', ()=>{
 
 
 
+clear.addEventListener('click',()=>{
 
+    let gridPixels = container.querySelectorAll('cell');
+
+    gridPixels.forEach(item => item.style.backgroundColor = 'white');
+
+});
 
